11b13f9512b6f271e638fbc6b3473d6107a525c0
Oleg Ilyenko <oleg.ilyenko@gmail.com>
2015-07-24 17:12:00 +0200

    Moved example parsing

--- src/test/scala/sangria/util/TemporarySpec.scala
+++ src/test/scala/sangria/util/TemporarySpec.scala
@@ -4,7 +4,7 @@ import org.scalatest.{Matchers, WordSpec}
 import sangria.TestData.{FriendsResolver, CharacterRepo}
 import sangria.execution.Executor
 import sangria.parser.{SyntaxError, QueryParser}
-import sangria.renderer.SchemaRenderer
+import sangria.renderer.{QueryRenderer, SchemaRenderer}
 import sangria.schema.TestSchema
 import sangria.introspection.introspectionQuery
 
@@ -16,7 +16,46 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 class TemporarySpec extends WordSpec with Matchers {
 
-  "Foo" should {
+  "Just playing around" should {
+    "parsing" in {
+      val query =
+        """
+          query FetchLukeAndLeiaAliased($someVar: Int = 1.23,$anotherVar: Int = 123)@include(if: true) @include(if: false){
+            luke: human(id: "1000")@include(if: true){
+              friends(sort: NAME)
+            }
+            leia: human(id: "10103\n \u00F6 ö") {
+              name
+            }
+
+            ... on User {
+              birth{day}
+            }
+
+            ...Foo
+          }
+
+          fragment Foo on User @foo(bar: 1){
+            baz
+          }
+        """
+
+      val res = QueryParser.parse(query)
+
+      res match {
+        case Success(ast) =>
+          println("SUCCESS")
+          println(ast)
+
+          println(QueryRenderer.render(ast))
+          println(QueryRenderer.render(ast, QueryRenderer.Compact))
+        case Failure(error: SyntaxError) =>
+          println(error.message)
+        case Failure(error) =>
+          error.printStackTrace()
+      }
+    }
+
     "values and defers" in {
       val ast = QueryParser.parse(
         s"""
