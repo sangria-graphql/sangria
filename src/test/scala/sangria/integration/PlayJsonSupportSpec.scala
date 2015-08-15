1f9c5a8f010083875025245a3782b7c8428f84c0
Oleg Ilyenko <oleg.ilyenko@gmail.com>
2015-08-15 19:41:07 +0200

    More ResultMarshaller tests

--- src/test/scala/sangria/integration/PlayJsonSupportSpec.scala
+++ src/test/scala/sangria/integration/PlayJsonSupportSpec.scala
@@ -1,7 +1,9 @@
 package sangria.integration
 
 import org.scalatest.{Matchers, WordSpec}
+import play.api.libs.json._
 import sangria.execution.Executor
+import sangria.integration.PlayJsonSupport.PlayJsonResultMarshaller
 import sangria.parser.QueryParser
 import sangria.starWars.TestData.{CharacterRepo, FriendsResolver}
 import sangria.starWars.TestSchema._
@@ -14,7 +16,6 @@ class PlayJsonSupportSpec extends WordSpec with Matchers with AwaitSupport {
   "Json4sSupport" should {
     "Marshal and Unmarshal" in {
       import sangria.integration.PlayJsonSupport._
-      import play.api.libs.json._
 
       val Success(query) = QueryParser.parse("""
         query FetchSomeIDQuery($someId: String!) {
@@ -37,6 +38,31 @@ class PlayJsonSupportSpec extends WordSpec with Matchers with AwaitSupport {
           |    }
           |  }
           |}""".stripMargin)
+    }
+    
+    "correctly serialize" in {
+      PlayJsonResultMarshaller.addArrayNodeElem(JsArray(List(JsString("aa"))), JsString("bb")) should be (
+        JsArray(List(JsString("aa"), JsString("bb"))))
+
+      PlayJsonResultMarshaller.emptyArrayNode should be (JsArray(Nil))
+      PlayJsonResultMarshaller.isEmptyArrayNode(JsArray(Nil)) should be (true)
+      PlayJsonResultMarshaller.isEmptyArrayNode(JsArray(JsString("aa") :: Nil)) should be (false)
+      PlayJsonResultMarshaller.arrayNode(JsString("aa") :: JsNumber(11) :: Nil) should be (JsArray(JsString("aa") :: JsNumber(11) :: Nil))
+
+      PlayJsonResultMarshaller.booleanNode(true) should be (JsBoolean(true))
+      PlayJsonResultMarshaller.intNode(111) should be (JsNumber(111))
+      PlayJsonResultMarshaller.floatNode(111.22D) should be (JsNumber(111.22D))
+      PlayJsonResultMarshaller.stringNode("qq") should be (JsString("qq"))
+      PlayJsonResultMarshaller.bigIntNode(BigInt("12323432432432")) should be (JsNumber(BigDecimal("12323432432432")))
+      PlayJsonResultMarshaller.bigDecimalNode(BigDecimal("12323432432432.2435454354543")) should be (JsNumber(BigDecimal("12323432432432.2435454354543")))
+
+      PlayJsonResultMarshaller.emptyMapNode should be (JsObject(Nil))
+      PlayJsonResultMarshaller.addMapNodeElem(JsObject(Seq("aa" -> JsString("bb"))), "cc", JsNumber(321)) should be (
+        JsObject(Seq("aa" -> JsString("bb"), "cc" -> JsNumber(321))))
+      PlayJsonResultMarshaller.mapNode("aa" -> JsString("bb") :: "cc" -> JsNumber(321) :: Nil) should be (
+        JsObject(Seq("aa" -> JsString("bb"), "cc" -> JsNumber(321))))
+
+      PlayJsonResultMarshaller.nullNode should be (JsNull)
     }
   }
 }
--- src/test/scala/sangria/integration/SprayJsonSupportSpec.scala
+++ src/test/scala/sangria/integration/SprayJsonSupportSpec.scala
@@ -2,10 +2,13 @@ package sangria.integration
 
 import org.scalatest.{Matchers, WordSpec}
 import sangria.execution.Executor
+import sangria.integration.SprayJsonSupport.SprayJsonResultMarshaller
 import sangria.parser.QueryParser
 import sangria.starWars.TestData.{CharacterRepo, FriendsResolver}
 import sangria.starWars.TestSchema._
 import sangria.util.AwaitSupport
+
+import spray.json._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.Success
@@ -13,7 +16,6 @@ import scala.util.Success
 class SprayJsonSupportSpec extends WordSpec with Matchers with AwaitSupport {
   "Json4sSupport" should {
     "Marshal and Unmarshal" in {
-      import spray.json._
       import sangria.integration.SprayJsonSupport._
 
       val Success(query) = QueryParser.parse("""
@@ -37,6 +39,31 @@ class SprayJsonSupportSpec extends WordSpec with Matchers with AwaitSupport {
           |    }
           |  }
           |}""".stripMargin)
+    }
+    
+    "correctly serialize" in {
+      SprayJsonResultMarshaller.addArrayNodeElem(JsArray(Vector(JsString("aa"))), JsString("bb")) should be (
+        JsArray(Vector(JsString("aa"), JsString("bb"))))
+
+      SprayJsonResultMarshaller.emptyArrayNode should be (JsArray(Vector.empty))
+      SprayJsonResultMarshaller.isEmptyArrayNode(JsArray(Vector.empty)) should be (true)
+      SprayJsonResultMarshaller.isEmptyArrayNode(JsArray(Vector(JsString("aa")))) should be (false)
+      SprayJsonResultMarshaller.arrayNode(JsString("aa") :: JsNumber(11) :: Nil) should be (JsArray(Vector(JsString("aa"), JsNumber(11))))
+
+      SprayJsonResultMarshaller.booleanNode(true) should be (JsBoolean(true))
+      SprayJsonResultMarshaller.intNode(111) should be (JsNumber(111))
+      SprayJsonResultMarshaller.floatNode(111.22D) should be (JsNumber(111.22D))
+      SprayJsonResultMarshaller.stringNode("qq") should be (JsString("qq"))
+      SprayJsonResultMarshaller.bigIntNode(BigInt("12323432432432")) should be (JsNumber(BigDecimal("12323432432432")))
+      SprayJsonResultMarshaller.bigDecimalNode(BigDecimal("12323432432432.2435454354543")) should be (JsNumber(BigDecimal("12323432432432.2435454354543")))
+
+      SprayJsonResultMarshaller.emptyMapNode should be (JsObject.empty)
+      SprayJsonResultMarshaller.addMapNodeElem(JsObject(Map("aa" -> JsString("bb"))), "cc", JsNumber(321)) should be (
+        JsObject(Map("aa" -> JsString("bb"), "cc" -> JsNumber(321))))
+      SprayJsonResultMarshaller.mapNode("aa" -> JsString("bb") :: "cc" -> JsNumber(321) :: Nil) should be (
+        JsObject(Map("aa" -> JsString("bb"), "cc" -> JsNumber(321))))
+
+      SprayJsonResultMarshaller.nullNode should be (JsNull)
     }
   }
 }
