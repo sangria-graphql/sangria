07532d0b4011a4058149f8c0796244f72bbf498a
Oleg Ilyenko <oleg.ilyenko@gmail.com>
2016-10-16 00:32:55 +0200

    Input object + schema changes

--- src/main/scala/sangria/schema/SchemaComparator.scala
+++ src/main/scala/sangria/schema/SchemaComparator.scala
@@ -1,5 +1,8 @@
 package sangria.schema
 
+import sangria.execution.ValueCoercionHelper
+import sangria.renderer.SchemaRenderer
+
 import language.existentials
 
 object SchemaComparator {
@@ -16,7 +19,7 @@ object SchemaComparator {
     val added = newTypes.diff(oldTypes).toVector.map(name ⇒
       SchemaChange.TypeAdded(newSchema.types(name)._2))
 
-    val kindChange = oldTypes.intersect(newTypes).flatMap { name ⇒
+    val changed = oldTypes.intersect(newTypes).flatMap { name ⇒
       val oldType = oldSchema.types(name)._2
       val newType = newSchema.types(name)._2
 
@@ -26,13 +29,33 @@ object SchemaComparator {
         Vector(SchemaChange.TypeKindChanged(newType, oldType))
     }
 
-    removed ++ added ++ kindChange
+    removed ++ added ++ changed ++ findChangesInSchema(oldSchema, newSchema)
+  }
+
+  def findChangesInSchema(oldSchema: Schema[_, _], newSchema: Schema[_, _]): Vector[SchemaChange] = {
+    val withQuery =
+      if (oldSchema.query.name != newSchema.query.name)
+        Vector(SchemaChange.SchemaQueryTypeChanged(oldSchema.query, newSchema.query))
+      else
+        Vector.empty
+
+    val withMutation =
+      if (oldSchema.mutation.map(_.name) != newSchema.mutation.map(_.name))
+        withQuery :+ SchemaChange.SchemaMutationTypeChanged(oldSchema.mutation, newSchema.mutation)
+      else
+        withQuery
+
+    if (oldSchema.subscription.map(_.name) != newSchema.subscription.map(_.name))
+      withMutation :+ SchemaChange.SchemaSubscriptionTypeChanged(oldSchema.subscription, newSchema.subscription)
+    else
+      withMutation
   }
 
   def findChangesInTypes(oldType: Type with Named, newType: Type with Named): Vector[SchemaChange] = {
     val typeChanges = (oldType, newType) match {
       case (o: EnumType[_], n: EnumType[_]) ⇒ findInEnumTypes(o, n)
       case (o: UnionType[_], n: UnionType[_]) ⇒ findInUnionTypes(o, n)
+      case (o: InputObjectType[_], n: InputObjectType[_]) ⇒ findInInputObjectTypes(o, n)
       case _ ⇒ Vector.empty
     }
 
@@ -73,6 +96,63 @@ object SchemaComparator {
     removed ++ added ++ changed
   }
 
+  private def findInInputObjectTypes(oldType: InputObjectType[_], newType: InputObjectType[_]): Vector[SchemaChange] = {
+    val oldFields = oldType.fields.map(_.name).toSet
+    val newFields = newType.fields.map(_.name).toSet
+
+    val removed = oldFields.diff(newFields).toVector.map(name ⇒
+      SchemaChange.InputFieldRemoved(oldType, oldType.fieldsByName(name)))
+
+    val added = newFields.diff(oldFields).toVector.map { name ⇒
+      val field = newType.fieldsByName(name)
+
+      SchemaChange.InputFieldAdded(newType, field, !isOptional(field))
+    }
+
+    val changed = oldFields.intersect(newFields).flatMap { name ⇒
+      val oldField = oldType.fieldsByName(name)
+      val newField = newType.fieldsByName(name)
+
+      findDescriptionChanged(oldField, newField, SchemaChange.InputFieldDescriptionChanged(newType, newField, _, _)) ++
+        findInInputFields(oldType, newType, oldField, newField)
+    }
+
+    removed ++ added ++ changed
+  }
+
+  private def findInInputFields(oldType: InputObjectType[_], newType: InputObjectType[_], oldField: InputField[_], newField: InputField[_]): Vector[SchemaChange] = {
+    val oldDefault = oldField.defaultValue.flatMap(dv ⇒ DefaultValueRenderer.renderInputValueCompact(dv, oldField.fieldType, coercionHelper))
+    val newDefault = newField.defaultValue.flatMap(dv ⇒ DefaultValueRenderer.renderInputValueCompact(dv, newField.fieldType, coercionHelper))
+
+    val withDefault =
+      if (oldDefault != newDefault)
+        Vector(SchemaChange.InputFieldDefaultChanged(newType, newField, oldDefault, newDefault))
+      else
+        Vector.empty
+
+    val oldFieldType = SchemaRenderer.renderTypeName(oldField.fieldType)
+    val newFieldType = SchemaRenderer.renderTypeName(newField.fieldType)
+
+    val withType =
+      if (oldFieldType != newFieldType)
+        withDefault :+ SchemaChange.InputFieldTypeChanged(newType, newField, SchemaRenderer.renderTypeName(nonContainer(newField)) != oldFieldType,
+          oldField.fieldType, newField.fieldType)
+      else
+        withDefault
+
+    withType
+  }
+
+  private def isOptional(field: InputField[_]) = field.fieldType match {
+    case _: OptionInputType[_] ⇒ true
+    case _ ⇒ false
+  }
+
+  private def nonContainer(field: InputField[_]) = field.fieldType match {
+    case OptionInputType(ofType) ⇒ ofType
+    case tpe ⇒ tpe
+  }
+
   private def findDescriptionChanged(o: Named, n: Named, fn: (Option[String], Option[String]) ⇒ SchemaChange): Vector[SchemaChange] =
     if (o.description != n.description) Vector(fn(o.description, n.description))
     else Vector.empty
@@ -80,6 +160,8 @@ object SchemaComparator {
   private def findDeprecationChanged(o: HasDeprecation, n: HasDeprecation, fn: (Option[String], Option[String]) ⇒ SchemaChange): Vector[SchemaChange] =
     if (o.deprecationReason != n.deprecationReason) Vector(fn(o.deprecationReason, n.deprecationReason))
     else Vector.empty
+
+  private val coercionHelper = new ValueCoercionHelper[Any]
 }
 
 sealed trait SchemaChange {
@@ -102,47 +184,71 @@ object SchemaChange {
     def newDescription: Option[String]
   }
 
-  sealed abstract class BreakingChange(val description: String) extends SchemaChange {
-    final def breakingChange = true
-  }
+  sealed abstract class AbstractChange(val description: String, val breakingChange: Boolean) extends SchemaChange
 
-  case class TypeRemoved(tpe: Type with Named) extends BreakingChange(s"`${tpe.name}` type was removed") with TypeChange
+  // Breaking changes
+
+  case class TypeRemoved(tpe: Type with Named) extends AbstractChange(s"`${tpe.name}` type was removed", true) with TypeChange
 
   case class TypeKindChanged(tpe: Type with Named, oldTpe: Type with Named)
-    extends BreakingChange({
+    extends AbstractChange({
       val oldKind = kind(oldTpe)
       val newKind = kind(tpe)
 
       s"`${tpe.name}` changed from ${article(oldKind)} $oldKind type to ${article(newKind)} $newKind type"
-    }) with TypeChange
+    }, true) with TypeChange
 
   case class EnumValueRemoved(tpe: EnumType[_], value: EnumValue[_])
-    extends BreakingChange(s"Enum value `${value.name}` was removed from enum `${tpe.name}`") with TypeChange
+    extends AbstractChange(s"Enum value `${value.name}` was removed from enum `${tpe.name}`", true) with TypeChange
 
   case class UnionMemberRemoved(tpe: UnionType[_], member: ObjectType[_, _])
-    extends BreakingChange(s"`${member.name}` type was removed from union `${tpe.name}`") with TypeChange
+    extends AbstractChange(s"`${member.name}` type was removed from union `${tpe.name}`", true) with TypeChange
 
-  sealed abstract class NonBreakingChange(val description: String) extends SchemaChange {
-    final def breakingChange = false
-  }
+  case class InputFieldRemoved(tpe: InputObjectType[_], field: InputField[_])
+    extends AbstractChange(s"Input field `${field.name}` was removed from `${tpe.name}` type", true) with TypeChange
+
+  // Non-breaking changes
 
   case class TypeAdded(tpe: Type with Named)
-    extends NonBreakingChange(s"`${tpe.name}` type was added") with TypeChange
+    extends AbstractChange(s"`${tpe.name}` type was added", false) with TypeChange
 
   case class TypeDescriptionChanged(tpe: Type with Named, oldDescription: Option[String], newDescription: Option[String])
-    extends NonBreakingChange(s"`${tpe.name}` type description is changed") with DescriptionChange
+    extends AbstractChange(s"`${tpe.name}` type description is changed", false) with DescriptionChange
 
   case class EnumValueAdded(tpe: EnumType[_], value: EnumValue[_])
-    extends NonBreakingChange(s"Enum value `${value.name}` was added to enum `${tpe.name}`") with TypeChange
+    extends AbstractChange(s"Enum value `${value.name}` was added to enum `${tpe.name}`", false) with TypeChange
 
   case class EnumValueDescriptionChanged(tpe: EnumType[_], value: EnumValue[_], oldDescription: Option[String], newDescription: Option[String])
-      extends NonBreakingChange(s"`${tpe.name}.${value.name}` description changed") with DescriptionChange
+    extends AbstractChange(s"`${tpe.name}.${value.name}` description changed", false) with DescriptionChange
 
   case class EnumValueDeprecated(tpe: EnumType[_], value: EnumValue[_], oldDeprecationReason: Option[String], newDeprecationReason: Option[String])
-    extends NonBreakingChange(s"Enum value `${value.name}` was deprecated in enum `${tpe.name}`") with DeprecationChange
+    extends AbstractChange(s"Enum value `${value.name}` was deprecated in enum `${tpe.name}`", false) with DeprecationChange
 
   case class UnionMemberAdded(tpe: UnionType[_], member: ObjectType[_, _])
-    extends BreakingChange(s"`${member.name}` type was added to union `${tpe.name}`") with TypeChange
+    extends AbstractChange(s"`${member.name}` type was added to union `${tpe.name}`", false) with TypeChange
+
+  case class InputFieldDescriptionChanged(tpe: InputObjectType[_], field: InputField[_], oldDescription: Option[String], newDescription: Option[String])
+    extends AbstractChange(s"`${tpe.name}.${field.name}` description is changed", false) with DescriptionChange
+
+  case class InputFieldDefaultChanged(tpe: InputObjectType[_], field: InputField[_], oldDefault: Option[String], newDefault: Option[String])
+    extends AbstractChange(s"`${tpe.name}.${field.name}` default value changed from ${oldDefault.fold("none")("`" + _ + "`")} to ${newDefault.fold("none")("`" + _ + "`")}", false) with TypeChange
+
+  case class SchemaQueryTypeChanged(oldType: ObjectType[_, _], newType: ObjectType[_, _])
+    extends AbstractChange(s"Schema query type changed from `${oldType.name}` to `${newType.name}` type", true)
+
+  // May be breaking changes
+
+  case class InputFieldAdded(tpe: InputObjectType[_], field: InputField[_], breaking: Boolean)
+    extends AbstractChange(s"Input field `${field.name}` was added to `${tpe.name}` type", breaking) with TypeChange
+
+  case class InputFieldTypeChanged(tpe: InputObjectType[_], field: InputField[_], breaking: Boolean, oldFiledType: InputType[_], newFieldType: InputType[_])
+    extends AbstractChange(s"`${tpe.name}.${field.name}` input field type changed from `${SchemaRenderer.renderTypeName(oldFiledType)}` to `${SchemaRenderer.renderTypeName(newFieldType)}`", breaking) with TypeChange
+
+  case class SchemaMutationTypeChanged(oldType: Option[ObjectType[_, _]], newType: Option[ObjectType[_, _]])
+    extends AbstractChange(s"Schema mutation type changed from ${oldType.fold("none")(t ⇒ "`" + t.name + "`")} to ${oldType.fold("none")(t ⇒ "`" + t.name + "`")} type", oldType.nonEmpty)
+
+  case class SchemaSubscriptionTypeChanged(oldType: Option[ObjectType[_, _]], newType: Option[ObjectType[_, _]])
+    extends AbstractChange(s"Schema subscription type changed from ${oldType.fold("none")(t ⇒ "`" + t.name + "`")} to ${oldType.fold("none")(t ⇒ "`" + t.name + "`")} type", oldType.nonEmpty)
 
   private val AnArticleLetters = Set('a', 'e', 'i', 'o')
 
--- src/test/scala/sangria/schema/SchemaComparatorSpec.scala
+++ src/test/scala/sangria/schema/SchemaComparatorSpec.scala
@@ -25,10 +25,10 @@ class SchemaComparatorSpec extends WordSpec with Matchers {
       val newSchema = Schema(QueryType, additionalTypes = type2 :: Nil)
 
       assertChanges(newSchema.compare(oldSchema),
-        change[TypeRemoved]("`Type1` type was removed"))
+        breakingChange[TypeRemoved]("`Type1` type was removed"))
 
       assertChanges(oldSchema.compare(newSchema),
-        change[TypeAdded]("`Type1` type was added"))
+        nonBreakingChange[TypeAdded]("`Type1` type was added"))
 
       oldSchema.compare(oldSchema) should be (Vector.empty)
     }
@@ -43,8 +43,8 @@ class SchemaComparatorSpec extends WordSpec with Matchers {
         union Type1 = ObjectType
       """,
 
-      change[TypeAdded]("`ObjectType` type was added"),
-      change[TypeKindChanged]("`Type1` changed from an Interface type to a Union type"))
+      nonBreakingChange[TypeAdded]("`ObjectType` type was added"),
+      breakingChange[TypeKindChanged]("`Type1` changed from an Interface type to a Union type"))
 
     "detect if a type description changed " in checkChanges(
       graphql"""
@@ -57,7 +57,7 @@ class SchemaComparatorSpec extends WordSpec with Matchers {
         type ObjectType {field1: String}
       """,
 
-      change[TypeDescriptionChanged]("`ObjectType` type description is changed"))
+      nonBreakingChange[TypeDescriptionChanged]("`ObjectType` type description is changed"))
 
     "should detect changes in enum values" in checkChanges(
       graphql"""
@@ -76,10 +76,10 @@ class SchemaComparatorSpec extends WordSpec with Matchers {
         }
       """,
 
-      change[EnumValueRemoved]("Enum value `A` was removed from enum `Foo`"),
-      change[EnumValueAdded]("Enum value `D` was added to enum `Foo`"),
-      change[EnumValueDescriptionChanged]("`Foo.C` description changed"),
-      change[EnumValueDeprecated]("Enum value `B` was deprecated in enum `Foo`"))
+      breakingChange[EnumValueRemoved]("Enum value `A` was removed from enum `Foo`"),
+      nonBreakingChange[EnumValueAdded]("Enum value `D` was added to enum `Foo`"),
+      nonBreakingChange[EnumValueDescriptionChanged]("`Foo.C` description changed"),
+      nonBreakingChange[EnumValueDeprecated]("Enum value `B` was deprecated in enum `Foo`"))
 
     "should detect changes in unions" in checkChanges(
       graphql"""
@@ -96,17 +96,133 @@ class SchemaComparatorSpec extends WordSpec with Matchers {
         union Agg = Bar | Baz
       """,
 
-      change[TypeAdded]("`Baz` type was added"),
-      change[TypeRemoved]("`Foo` type was removed"),
-      change[UnionMemberRemoved]("`Foo` type was removed from union `Agg`"),
-      change[UnionMemberAdded]("`Baz` type was added to union `Agg`"),
-      change[TypeDescriptionChanged]("`Agg` type description is changed"))
+      nonBreakingChange[TypeAdded]("`Baz` type was added"),
+      breakingChange[TypeRemoved]("`Foo` type was removed"),
+      breakingChange[UnionMemberRemoved]("`Foo` type was removed from union `Agg`"),
+      nonBreakingChange[UnionMemberAdded]("`Baz` type was added to union `Agg`"),
+      nonBreakingChange[TypeDescriptionChanged]("`Agg` type description is changed"))
+
+    "should detect changes in scalars" in checkChanges(
+      graphql"""
+        scalar Date
+        scalar Locale
+      """,
+
+      graphql"""
+        # This is locale
+        scalar Locale
+
+        # This is country
+        scalar Country
+      """,
+
+      breakingChange[TypeRemoved]("`Date` type was removed"),
+      nonBreakingChange[TypeAdded]("`Country` type was added"),
+      nonBreakingChange[TypeDescriptionChanged]("`Locale` type description is changed"))
+
+    "should detect changes in input types" in checkChanges(
+      graphql"""
+        input Sort {dir: Int}
+        input Bar {size: Int}
+      """,
+
+      graphql"""
+        # This is sort
+        input Sort {dir: Int}
+
+        # This is foo
+        input Foo {size: Int}
+      """,
+
+      breakingChange[TypeRemoved]("`Bar` type was removed"),
+      nonBreakingChange[TypeAdded]("`Foo` type was added"),
+      nonBreakingChange[TypeDescriptionChanged]("`Sort` type description is changed"))
+
+    "detect changes in input type fields when they are added or removed" in checkChanges(
+      graphql"""
+        input Filter {
+          name: String!
+          descr: String
+        }
+      """,
+
+      graphql"""
+        # search filter
+        input Filter {
+          # filter by name
+          name: String!
+
+          # filter by size
+          size: Int
+        }
+      """,
+
+      breakingChange[InputFieldRemoved]("Input field `descr` was removed from `Filter` type"),
+      nonBreakingChange[InputFieldAdded]("Input field `size` was added to `Filter` type"),
+      nonBreakingChange[InputFieldDescriptionChanged]("`Filter.name` description is changed"),
+      nonBreakingChange[TypeDescriptionChanged]("`Filter` type description is changed"))
+
+    "detect changes in input type fields default value changes" in checkChanges(
+      graphql"""
+        input Filter {
+          a: [String!] = ["hello", "world"]
+          size: Int
+          color: Int = 5
+          type: [Int] = [1, 2, 3]
+        }
+      """,
+
+      graphql"""
+        input Filter {
+          a: [String!] = ["foo"]
+          size: Int = 12
+          color: String = "red"
+          type: [Int!] = [1, 2, 3]
+        }
+      """,
+
+      breakingChange[InputFieldTypeChanged]("""`Filter.color` input field type changed from `Int` to `String`"""),
+      breakingChange[InputFieldTypeChanged]("""`Filter.type` input field type changed from `[Int]` to `[Int!]`"""),
+      nonBreakingChange[InputFieldDefaultChanged]("""`Filter.a` default value changed from `["hello","world"]` to `["foo"]`"""),
+      nonBreakingChange[InputFieldDefaultChanged]("`Filter.size` default value changed from none to `12`"),
+      nonBreakingChange[InputFieldDefaultChanged]("""`Filter.color` default value changed from `5` to `"red"`"""))
+
+    "detect breaking and non-breaking changes in input type fields type" in checkChanges(
+      graphql"""
+        input Filter {
+          a: String!
+          b: String
+          b1: String
+          c: [String]
+        }
+      """,
+
+      graphql"""
+        input Filter {
+          a: String
+          b: [String]
+          b1: String!
+          c: [String]!
+          d: Int
+          e: Int!
+        }
+      """,
+
+      breakingChange[InputFieldAdded]("Input field `e` was added to `Filter` type"),
+      nonBreakingChange[InputFieldAdded]("Input field `d` was added to `Filter` type"),
+      breakingChange[InputFieldTypeChanged]("`Filter.b` input field type changed from `String` to `[String]`"),
+      nonBreakingChange[InputFieldTypeChanged]("`Filter.a` input field type changed from `String!` to `String`"),
+      breakingChange[InputFieldTypeChanged]("`Filter.c` input field type changed from `[String]` to `[String]!`"),
+      breakingChange[InputFieldTypeChanged]("`Filter.b1` input field type changed from `String` to `String!`"))
   }
 
-  def change[T : ClassTag](description: String) =
-    implicitly[ClassTag[T]].runtimeClass → description
+  def breakingChange[T : ClassTag](description: String) =
+    (implicitly[ClassTag[T]].runtimeClass, description, true)
 
-  def checkChanges(oldDoc: Document, newDoc: Document, expectedChanges: (Class[_], String)*) = {
+  def nonBreakingChange[T : ClassTag](description: String) =
+    (implicitly[ClassTag[T]].runtimeClass, description, false)
+
+  def checkChanges(oldDoc: Document, newDoc: Document, expectedChanges: (Class[_], String, Boolean)*) = {
     val queryType =
       graphql"""
         type Query {
@@ -120,17 +236,17 @@ class SchemaComparatorSpec extends WordSpec with Matchers {
     assertChanges(newSchema.compare(oldSchema), expectedChanges: _*)
   }
 
-  def assertChanges(actualChanges: Vector[SchemaChange], expectedChanges: (Class[_], String)*) = {
-    val actualRendered = actualChanges.map(c ⇒ s"  * ${c.getClass.getSimpleName}: ${c.description}").mkString("\n")
+  def assertChanges(actualChanges: Vector[SchemaChange], expectedChanges: (Class[_], String, Boolean)*) = {
+    val actualRendered = actualChanges.map(c ⇒ s"  * ${c.getClass.getSimpleName}: ${c.description}${if (c.breakingChange) " (breaking)" else ""}").mkString("\n")
 
     withClue(s"Actual changes:\n$actualRendered\n") {
       actualChanges should have size expectedChanges.size
 
       val notFound = expectedChanges.filter(expectedChange ⇒
-        !actualChanges.exists(ac ⇒ expectedChange._1.isAssignableFrom(ac.getClass) && ac.description == expectedChange._2))
+        !actualChanges.exists(ac ⇒ expectedChange._1.isAssignableFrom(ac.getClass) && ac.description == expectedChange._2 && ac.breakingChange == expectedChange._3))
 
       if (notFound.nonEmpty) {
-        val str = notFound.map(nf ⇒ s"  * ${nf._1.getSimpleName}: ${nf._2}").mkString("\n")
+        val str = notFound.map(nf ⇒ s"  * ${nf._1.getSimpleName}: ${nf._2}${if (nf._3) " (breaking)" else ""}").mkString("\n")
 
         fail(s"Changes not found:\n $str")
       }
