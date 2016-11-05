25139c011bddc7bbbc29cde015f0477b636eb06b
Oleg Ilyenko <oleg.ilyenko@gmail.com>
2016-11-05 18:36:56 +0100

    increased timeouts even more

--- src/test/scala/sangria/util/FutureResultSupport.scala
+++ src/test/scala/sangria/util/FutureResultSupport.scala
@@ -10,16 +10,16 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 trait FutureResultSupport {
   implicit class FutureResult[T](f: Future[T]) {
-    def await = Await.result(f, 3 seconds)
+    def await = Await.result(f, 10 seconds)
     def await(duration: Duration) = Await.result(f, duration)
 
-    def awaitAndRecoverQueryAnalysis(implicit m: ResultMarshallerForType[T]): T = Await.result(recoverQueryAnalysis, 5 seconds)
+    def awaitAndRecoverQueryAnalysis(implicit m: ResultMarshallerForType[T]): T = Await.result(recoverQueryAnalysis, 10 seconds)
 
     def recoverQueryAnalysis(implicit m: ResultMarshallerForType[T]): Future[T] = f.recover {
       case analysisError: QueryAnalysisError ⇒ analysisError.resolveError(m.marshaller).asInstanceOf[T]
     }
 
-    def awaitAndRecoverQueryAnalysisScala(implicit ev: T =:= Any) = Await.result(recoverQueryAnalysisScala, 5 seconds)
+    def awaitAndRecoverQueryAnalysisScala(implicit ev: T =:= Any) = Await.result(recoverQueryAnalysisScala, 10 seconds)
 
     def recoverQueryAnalysisScala(implicit ev: T =:= Any) = f.recover {
       case analysisError: ErrorWithResolver ⇒ analysisError.resolveError
