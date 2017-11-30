6738d71ea27a646366629079efab980d8975037a
Oleg Ilyenko <oleg.ilyenko@gmail.com>
2017-12-01 00:38:42 +0100

    Implemented `blockStringValue`

--- src/main/scala/sangria/util/StringUtil.scala
+++ src/main/scala/sangria/util/StringUtil.scala
@@ -93,4 +93,35 @@ object StringUtil {
 
   def charHex(ch: Char): String =
     Integer.toHexString(ch).toUpperCase(Locale.ENGLISH)
+
+  /**
+    * Produces the value of a block string from its parsed raw value, similar to
+    * Coffeescript's block string, Python's docstring trim or Ruby's strip_heredoc.
+    *
+    * This implements the GraphQL spec's BlockStringValue() static algorithm.
+    */
+  def blockStringValue(rawString: String): String = {
+    val lines = rawString.split("""\r\n|[\n\r]""")
+    val lineSizes = lines.map(l ⇒ l → leadingWhitespace(l))
+    val commonIndentLines = lineSizes.drop(1).collect {case (line, size) if size != line.length ⇒ size}
+    val strippedLines =
+      if (commonIndentLines.nonEmpty) {
+        val commonIndent = commonIndentLines.min
+
+        lines.take(1) ++ lines.drop(1).map(_.drop(commonIndent))
+      } else lines
+    val trimmedLines = strippedLines.reverse.dropWhile(isBlank).reverse.dropWhile(isBlank)
+
+    trimmedLines.mkString("\n")
+  }
+
+  private def leadingWhitespace(str: String) =  {
+    var i = 0
+
+    while (i < str.length && (str.charAt(i) == ' ' || str.charAt(i) == '\t')) i += 1
+
+    i
+  }
+
+  private def isBlank(str: String) = leadingWhitespace(str) == str.length
 }
