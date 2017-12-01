7039eeacbe8677d5832bebff4b8c9cf1bbf1dce1
Alexis Mas <alexis.mas@xing.com>
2017-12-01 12:28:28 +0100

    Add test case for inconsistent enum type coercion

---
+++ src/test/scala/sangria/marshalling/EnumInputTypeSpec.scala
@@ -0,0 +1,105 @@
+package sangria.marshalling
+
+import java.util.concurrent.Executor
+
+import org.scalatest.{Matchers, WordSpec}
+import sangria.marshalling.sprayJson.SprayJsonInputUnmarshaller
+import sangria.parser.QueryParser
+import sangria.schema.{Argument, EnumType, EnumValue, Field, InputField, InputObjectType, ObjectType}
+import spray.json.{JsObject, JsString, JsValue, JsonReader}
+
+import scala.concurrent.duration._
+import scala.concurrent.{Await, ExecutionContext}
+import scala.util.Success
+
+class EnumInputTypeSpec extends WordSpec with Matchers {
+  implicit val inputUnmarshaller: SprayJsonInputUnmarshaller.type = SprayJsonInputUnmarshaller
+
+  implicit def sprayJsonFromInput[T <: JsValue]: FromInput[T] = sprayJson.sprayJsonFromInput[T]
+
+  implicit def sprayJsonReaderFromInput[T: JsonReader]: FromInput[T] = sprayJson.sprayJsonReaderFromInput[T]
+
+  private val enumWithCustomValueType = EnumType[String](
+    "CustomEnum",
+    Some("Some custom enum values"),
+    List(
+      EnumValue("TOP", value = "TOP_VALUE"),
+      EnumValue("OTHER", value = "OTHER_VALUE")
+    )
+  )
+
+  private val complexInputType = InputObjectType[JsObject]("customInput", fields = List(InputField("enumValue", enumWithCustomValueType)))
+  private val complexArgument  = Argument("complexArgument", complexInputType)
+  private val enumArgument     = Argument("simpleArgument", enumWithCustomValueType)
+
+  private val valueFromComplexField: Field[Unit, JsValue] = {
+    Field(
+      "valueFromComplex",
+      sangria.schema.StringType,
+      arguments = List(complexArgument),
+      resolve = { ctx =>
+        ctx.arg(complexArgument).getFields("enumValue").toString()
+      }
+    )
+  }
+
+  private val valueFromEnumField: Field[Unit, JsValue] = {
+    Field(
+      "valueFromEnum",
+      sangria.schema.StringType,
+      arguments = List(enumArgument),
+      resolve = { ctx =>
+        JsString(ctx.arg(enumArgument)).value
+      }
+    )
+  }
+
+  val queryType: ObjectType[Unit, JsValue] = ObjectType(
+    "query",
+    sangria.schema.fields(valueFromComplexField, valueFromEnumField)
+  )
+
+  val schema = new sangria.schema.Schema(queryType)
+
+  implicit val synchronousExecutionContext: ExecutionContext = ExecutionContext.fromExecutor(new Executor {
+    override def execute(command: Runnable): Unit = {
+      command.run()
+    }
+  })
+
+  private def runQuery(query: String) = {
+    val Success(queryAst) = QueryParser.parse(query)
+
+    val fut = sangria.execution.Executor
+      .execute(
+        schema,
+        queryAst,
+        userContext = (),
+        root = JsObject.empty
+      )
+
+    Await.result(fut, 2.seconds)
+  }
+
+  "correctly unmarshals an enum in a complex input type" in {
+    val query =
+      """
+        | query GetTheComplexThing {
+        |   valueFromComplex(complexArgument: { enumValue: TOP })
+        | }
+      """.stripMargin
+
+    runQuery(query) should be (Map("data" -> Map("valueFromComplex" -> "TOP_VALUE")))
+  }
+
+  "correctly unmarshals an enum passed as an argument" in {
+    val query =
+      """
+        | query GetTheSimpleThing {
+        |   valueFromEnum(simpleArgument: TOP)
+        | }
+      """.stripMargin
+
+    runQuery(query) should be (Map("data" -> Map("valueFromEnum" -> "TOP_VALUE")))
+  }
+}
