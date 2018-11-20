4e7ae9b916a8d8427b184342417599eeefbf004d
Alexis Mas <alexis.mas@xing.com>
2018-11-20 08:46:09 +0100

    Filter fields on interfaces

--- src/main/scala/sangria/schema/transformations/FieldFilteredAstSchemaBuilder.scala
+++ src/main/scala/sangria/schema/transformations/FieldFilteredAstSchemaBuilder.scala
@@ -1,11 +1,12 @@
 package sangria.schema.transformations
 
 import sangria.ast
-import sangria.schema.transformations.TransformSchema.FieldFilter
+import sangria.ast.InterfaceTypeExtensionDefinition
 import sangria.schema.{AstSchemaMaterializer, DefaultAstSchemaBuilder, Field, InterfaceType, MatOrigin, ObjectType}
 
 
-class FieldFilteredAstSchemaBuilder[Ctx](filter: FieldFilter[Ctx]) extends DefaultAstSchemaBuilder[Ctx] {
+class FieldFilteredAstSchemaBuilder[Ctx](filter: List[Field[Ctx, Any]] => List[Field[Ctx, Any]]) extends DefaultAstSchemaBuilder[Ctx] {
+
   override def extendObjectType(
                         origin: MatOrigin,
                         existing: ObjectType[Ctx, _],
@@ -13,14 +14,19 @@ class FieldFilteredAstSchemaBuilder[Ctx](filter: FieldFilter[Ctx]) extends Defau
                         fields: () ⇒ List[Field[Ctx, Any]],
                         interfaces: List[InterfaceType[Ctx, Any]],
                         mat: AstSchemaMaterializer[Ctx]): ObjectType[Ctx, Any] = {
-    super.extendObjectType(
-      origin = origin,
-      existing = existing,
-      extensions = extensions,
-      fields = () => filter(fields()),
-      interfaces = interfaces,
-      mat = mat
-    )
+    super.extendObjectType(origin, existing, extensions, applyFilter(fields), interfaces, mat)
+  }
+
+  override def extendInterfaceType(origin: MatOrigin,
+                                   existing: InterfaceType[Ctx, _],
+                                   extensions: List[InterfaceTypeExtensionDefinition],
+                                   fields: () => List[Field[Ctx, Any]],
+                                   mat: AstSchemaMaterializer[Ctx]): InterfaceType[Ctx, Any] = {
+    super.extendInterfaceType(origin, existing, extensions, applyFilter(fields), mat)
+  }
+
+  def applyFilter(fieldsFn: () => List[Field[Ctx, Any]]): () => List[Field[Ctx, Any]] = {
+    () => filter(fieldsFn())
   }
 }
 
--- src/main/scala/sangria/schema/transformations/TransformSchema.scala
+++ src/main/scala/sangria/schema/transformations/TransformSchema.scala
@@ -4,9 +4,8 @@ import sangria.ast.{Document, ObjectTypeExtensionDefinition}
 import sangria.schema.{AstSchemaMaterializer, Field, Schema}
 
 object TransformSchema {
-  type FieldFilter[Ctx] = List[Field[Ctx, Any]] => List[Field[Ctx, Any]]
 
-  def filterFields[Ctx, Val](schema: Schema[Ctx, Val], filter: FieldFilter[Ctx]): Schema[Ctx, Val] = {
+  def filterFields[Ctx, Val](schema: Schema[Ctx, Val], filter: List[Field[Ctx, Any]] => List[Field[Ctx, Any]]): Schema[Ctx, Val] = {
     AstSchemaMaterializer.extendSchema(
       schema,
       emptyExtensionDocument(schema.query.name),
