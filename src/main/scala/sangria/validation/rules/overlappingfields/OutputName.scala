4ff335bffccee6d5eed7e3e72646b01db90ac936
Simon Adameit <simon@simonadameit.com>
2019-11-14 14:27:07 +0100

    Update src/main/scala/sangria/validation/rules/overlappingfields/OutputName.scala
    Co-Authored-By: Yann Simon <yann.simon.fr@gmail.com>

--- src/main/scala/sangria/validation/rules/overlappingfields/OutputName.scala
+++ src/main/scala/sangria/validation/rules/overlappingfields/OutputName.scala
@@ -2,5 +2,5 @@ package sangria.validation.rules.overlappingfields
 
 class OutputName(val name: String) extends AnyVal {
 
-  override def toString: String = name.toString
+  override def toString: String = name
 }
