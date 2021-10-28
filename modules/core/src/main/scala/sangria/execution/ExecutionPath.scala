d5869aabdf0f6c04557a6b92020a175d2e594692
Yann Simon <yann.simon@commercetools.com>
2021-10-28 12:00:01 +0200

    less allocation in ExecutionPath.add
    fix https://github.com/sangria-graphql/sangria/issues/768

--- modules/core/src/main/scala/sangria/execution/ExecutionPath.scala
+++ modules/core/src/main/scala/sangria/execution/ExecutionPath.scala
@@ -4,33 +4,35 @@ import sangria.marshalling.ResultMarshaller
 import sangria.ast
 import sangria.schema.ObjectType
 
-case class ExecutionPath private (path: Vector[Any], cacheKeyPath: ExecutionPath.PathCacheKey) {
+class ExecutionPath private (_path: List[Any], cacheKeyPath: ExecutionPath.PathCacheKey) {
+  lazy val path: List[Any] = _path.reverse
+
   def add(field: ast.Field, parentType: ObjectType[_, _]) =
-    new ExecutionPath(path :+ field.outputName, cacheKey :+ field.outputName :+ parentType.name)
+    new ExecutionPath(field.outputName :: _path, parentType.name :: field.outputName :: cacheKey)
 
-  def withIndex(idx: Int) = new ExecutionPath(path :+ idx, cacheKey)
+  def withIndex(idx: Int) = new ExecutionPath(idx :: _path, cacheKey)
 
-  def isEmpty = path.isEmpty
-  def nonEmpty = path.nonEmpty
+  def isEmpty = _path.isEmpty
+  def nonEmpty = _path.nonEmpty
 
   /** @return
     *   last index in the path, if available
     */
-  def lastIndex: Option[Int] = path.lastOption.collect { case i: Int => i }
+  def lastIndex: Option[Int] = _path.headOption.collect { case i: Int => i }
 
   /** @return
     *   the size of the path excluding the indexes
     */
   def size = cacheKeyPath.size / 2
 
-  def marshal(m: ResultMarshaller): m.Node = m.arrayNode(path.map {
+  def marshal(m: ResultMarshaller): m.Node = m.arrayNode(_path.reverse.iterator.map {
     case s: String => m.scalarNode(s, "String", Set.empty)
     case i: Int => m.scalarNode(i, "Int", Set.empty)
-  })
+  }.toVector)
 
   def cacheKey: ExecutionPath.PathCacheKey = cacheKeyPath
 
-  override def toString = path.foldLeft("") {
+  override def toString = _path.reverse.foldLeft("") {
     case ("", str: String) => str
     case (acc, str: String) => acc + "." + str
     case (acc, idx: Int) => acc + "[" + idx + "]"
@@ -41,7 +43,7 @@ case class ExecutionPath private (path: Vector[Any], cacheKeyPath: ExecutionPath
 }
 
 object ExecutionPath {
-  type PathCacheKey = Vector[String]
+  type PathCacheKey = List[String]
 
-  val empty = new ExecutionPath(Vector.empty, Vector.empty)
+  val empty = new ExecutionPath(List.empty, List.empty)
 }
