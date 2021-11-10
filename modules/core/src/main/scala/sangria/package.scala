dff63b9ebd1b96ebc2027db6a4a3578e7c544527
Performant Data <performantdata@users.noreply.github.com>
2021-11-10 07:12:26 -0800

    remove references to other libraries
    Co-authored-by: Yann Simon <yann.simon.fr@gmail.com>

--- modules/core/src/main/scala/sangria/package.scala
+++ modules/core/src/main/scala/sangria/package.scala
@@ -3,11 +3,10 @@
   * Sangria is a library that provides parsing, validation, execution and other services for GraphQL
   * queries.
   *
-  * It typically requires other libraries to build a complete GraphQL service—including, perhaps,
-  * ones that provide a service interface (e.g.,
-  * [[https://doc.akka.io/docs/akka-http/current/index.html Akka HTTP]]) and a database interface
-  * (e.g., [[https://scala-slick.org/ Slick]])—as well as custom code to bridge the gap between data
-  * representations that are natural to the database vs. to the GraphQL schema.
+  * It typically requires other libraries to build a complete GraphQL service
+  * —including, perhaps, ones that provide a HTTP service interface and a database interface—
+  * as well as custom code to bridge the gap between data representations that are natural
+  * to the database vs. to the GraphQL schema.
   *
   * @see
   *   [[https://sangria-graphql.github.io/ the Sangria home page]]
