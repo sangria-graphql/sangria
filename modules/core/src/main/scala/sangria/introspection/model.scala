2d66de719aa9f734bb72b681b50195510fc9b979
Mike <performantdata@users.noreply.github.com>
2021-11-22 23:50:48 -0800

    scalafmt

--- modules/core/src/main/scala/sangria/introspection/model.scala
+++ modules/core/src/main/scala/sangria/introspection/model.scala
@@ -13,10 +13,16 @@ case class IntrospectionSchema(
     directives: Seq[IntrospectionDirective],
     description: Option[String]
 ) {
-  @deprecated("Removed in 3.0. Use SchemaRenderer.schemaAstFromIntrospection(•) instead.", since2_1_7)
+  @deprecated(
+    "Removed in 3.0. Use SchemaRenderer.schemaAstFromIntrospection(•) instead.",
+    since2_1_7
+  )
   def toAst = SchemaRenderer.schemaAstFromIntrospection(this)
 
-  @deprecated("Removed in 3.0. Use SchemaRenderer.schemaAstFromIntrospection(•, filter) instead.", since2_1_7)
+  @deprecated(
+    "Removed in 3.0. Use SchemaRenderer.schemaAstFromIntrospection(•, filter) instead.",
+    since2_1_7
+  )
   def toAst(filter: SchemaFilter): Document =
     SchemaRenderer.schemaAstFromIntrospection(this, filter)
 
@@ -26,10 +32,16 @@ case class IntrospectionSchema(
   @deprecated("Removed in 3.0. Use SchemaRenderer.renderSchema(•, filter) instead.", since2_1_7)
   def renderPretty(filter: SchemaFilter): String = toAst(filter).renderPretty
 
-  @deprecated("Removed in 3.0. Use QueryRenderer.renderCompact(SchemaRenderer.schemaAstFromIntrospection(•)) instead.", since2_1_7)
+  @deprecated(
+    "Removed in 3.0. Use QueryRenderer.renderCompact(SchemaRenderer.schemaAstFromIntrospection(•)) instead.",
+    since2_1_7
+  )
   def renderCompact: String = toAst.renderCompact
 
-  @deprecated("Removed in 3.0. Use QueryRenderer.renderCompact(SchemaRenderer.schemaAstFromIntrospection(•, filter)) instead.", since2_1_7)
+  @deprecated(
+    "Removed in 3.0. Use QueryRenderer.renderCompact(SchemaRenderer.schemaAstFromIntrospection(•, filter)) instead.",
+    since2_1_7
+  )
   def renderCompact(filter: SchemaFilter): String = toAst(filter).renderCompact
 
   lazy val typesByName = types.groupBy(_.name).map { case (k, v) => (k, v.head) }
