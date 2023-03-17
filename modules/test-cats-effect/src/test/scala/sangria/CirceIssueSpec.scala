0cfc8d12005ca8e3622199e06fade71ec18bb5c6
Yann Simon <yann.simon@commercetools.com>
2023-03-17 19:40:29 +0100

    give an array back

--- modules/test-cats-effect/src/test/scala/sangria/CirceIssueSpec.scala
+++ modules/test-cats-effect/src/test/scala/sangria/CirceIssueSpec.scala
@@ -8,7 +8,6 @@ import io.circe.literal._
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import sangria.execution.Executor
-import sangria.marshalling.InputUnmarshaller
 import sangria.util.FutureResultSupport
 
 class CirceIssueSpec extends AnyWordSpec with Matchers with FutureResultSupport {
@@ -25,7 +24,7 @@ class CirceIssueSpec extends AnyWordSpec with Matchers with FutureResultSupport 
           }
 
           type Query {
-            accounts: [Account!]!
+            accounts: [Account!]
           }
         """
 
@@ -51,29 +50,13 @@ class CirceIssueSpec extends AnyWordSpec with Matchers with FutureResultSupport 
               "name": "Darth Vader"
             }]"""
 
-      val response2 = List(
-        json"""
-      {
-        "id": "1000",
-        "name": "Luke Skywalker"
-      }""",
-        json"""
-      {
-        "id": "1001",
-        "name": "Darth Vader"
-      }"""
-      )
-
       val builder = AstSchemaBuilder.resolverBased[Unit](
         FieldResolver.map[Unit](
           "Query" -> Map(
-            "accounts" -> (_ => response)
+            "accounts" -> (_ => response.asArray)
           )
         ),
-        AnyFieldResolver[Unit] {
-          case origin if !origin.isInstanceOf[ExistingSchemaOrigin[_, _]] =>
-            extractFieldValue[Unit, Json]
-        }
+        AnyFieldResolver.defaultInput[Unit, Json]
       )
 
       val schema = Schema.buildFromAst(schemaAst, builder)
@@ -81,15 +64,6 @@ class CirceIssueSpec extends AnyWordSpec with Matchers with FutureResultSupport 
       val result = Executor.execute(schema, query)
       println(result.await)
     }
-  }
-
-  def extractFieldValue[Ctx, In](context: Context[Ctx, _])(implicit
-      iu: InputUnmarshaller[In]): Any = {
-    val parentType = context.parentType
-    val field = context.field
-    val value = context.value.asInstanceOf[In]
-    // there is not way to get a complete json object from iu
-    ResolverBasedAstSchemaBuilder.extractValue(field.fieldType, iu.getMapValue(value, field.name))
   }
 
 }
